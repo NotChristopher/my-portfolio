f19797c646fbb745d28976fd62144d9351d87b2c
Christopher Brummer <64840908+NotChristopher@users.noreply.github.com>
2024-09-12 21:47:56 +0200

    porfolio Update

---
+++ script.js
@@ -0,0 +1,6 @@
+const listItemProject = document.getElementById('Project');
+const listItemAbout = document.getElementById('About');
+
+const activeListItem = ()=>{
+    listItemAbout.style.background = "var(--secondary-color)";
+}
